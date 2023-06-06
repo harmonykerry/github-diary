e00b39dd5b75087915fb354aff0c4a53fb1fb1fa
harmonyk123 <harmonykerry@yahoo.com>
2023-06-06 01:46:39 +0100

    route

--- src/pages/MyRepos.js
+++ src/pages/MyRepos.js
@@ -1,29 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import Container from "react-bootstrap/esm/Container";
 import Banner from "../component/Banner";
 import ErrorMessage from "../component/ErrorMessage";
 import Repos from "../component/Repos";
-
-// const repos = [];
-
-const repos = undefined;
+import { getFromLocalStorage } from "../utils/getFromLocalStorage";
 
 function MyRepos() {
-  const removeRepo = () => {
-    console.log("remove.repo");
+  const [repos, setRepos] = useState(getFromLocalStorage("repos", []));
+
+  const removeRepo = (repoToRemove) => {
+    const reposFromLS = getFromLocalStorage("repos", []);
+    // console.log(repoToRemove);
+    // console.log("remove.repo");
+
+    const newRepos = reposFromLS.filter((repo) => {
+      return repo.id !== repoToRemove.id;
+    });
+
+    localStorage.setItem("repos", JSON.stringify(newRepos));
+
+    setRepos(newRepos);
   };
+
   return (
     <Container>
       <Banner
         title="My Repositories"
-        subTitle="Here is a list of my favorite Github Repositories"
+        subTitle="Here is a list of my favourite GitHub repositories"
       />
-
       {repos.length === 0 && (
-        <ErrorMessage message="You have no repositories in your favorites" />
+        <ErrorMessage message="You have no repositories in your favourites." />
       )}
       {repos.length > 0 && (
-        <Repos repos={repos} isFavorite={true} handleOnClick={removeRepo} />
+        <Repos repos={repos} isFavourite={true} handleOnClick={removeRepo} />
       )}
     </Container>
   );
